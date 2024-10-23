91737543a33119452ca4424c8e78200aca78c4d4
Jeremy Biagiotti <jbiagiotti8@gmail.com>
2024-10-23 16:18:09 -0400

    Initial Commit

---
+++ src/main/java/com/atomicaccountability/atomic_accountability/AtomicAccountabilityApplication.java
@@ -0,0 +1,13 @@
+package com.atomicaccountability.atomic_accountability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtomicAccountabilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtomicAccountabilityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atomicaccountability/atomic_accountability/AtomicAccountabilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atomicaccountability.atomic_accountability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtomicAccountabilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
